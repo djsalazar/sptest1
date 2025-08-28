ee1287f0796d933483c5fc29b4b3bf3e00cee34a
djsalazar <imdjsg@gmail.com>
2025-08-28 14:17:09 -0600

    N

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ DATA_DIR.mkdir(exist_ok=True)
 
 # Exam deadline (Guatemala timezone GMT-6)
 GUATEMALA_TZ = timezone(timedelta(hours=-6))
-EXAM_DEADLINE = datetime(2025, 8, 27, 23, 59, 0, tzinfo=GUATEMALA_TZ)
+EXAM_DEADLINE = datetime(2025, 9, 1, 23, 59, 0, tzinfo=GUATEMALA_TZ)
 
 # Flask setup
 app = Flask(__name__)
